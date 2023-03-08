69a69870f63990da976d871d8c3ebf48608836b3
GooseAkukaracha <103554799+GooseAkukaracha@users.noreply.github.com>
2023-03-08 23:45:06 +0300

    new

--- src/chess_elephant.java
+++ src/chess_elephant.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 public class chess_elephant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int slonX = sc.nextInt(), slonY = sc.nextInt();
+        int chessX = sc.nextInt(), chessY = sc.nextInt();
 
+        if ((chessX == slonX - 2 && chessY == slonY - 2) || (chessX == slonX + 2 && chessY == slonY + 2) || (chessX == slonX - 2 && chessY == slonY + 2) || (chessX == slonX + 2 && chessY == slonY - 2)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
     }
 }
